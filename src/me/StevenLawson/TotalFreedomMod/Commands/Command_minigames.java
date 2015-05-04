f4f7426d4144a1ce3170fa49d0e96c5deb36274a
Alex33856 <AlexFreedomMod@users.noreply.github.com>
2015-05-04 14:45:15 -0700

    Fixed the command

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_minigames.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_minigames.java
@@ -1,13 +1,13 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
-import me.StevenLawson.TotalFreedomMod.World.TFM_Flatlands;
+import me.StevenLawson.TotalFreedomMod.World.IFM_MinigameWorld;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Goto the flatlands.", usage = "/<command>")
+@CommandParameters(description = "Takes the sender to the minigame world", usage = "/<command>")
 public class Command_minigames extends TFM_Command
 {
     @Override
